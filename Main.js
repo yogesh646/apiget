c38d4b725f2c318f455d9f2fd00e035ed5873097
yogesh646 <122336939+yogesh646@users.noreply.github.com>
2023-01-10 16:18:15 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,101 +1,76 @@
-import React, { useEffect, useState } from 'react'
+import React,{ useState,useEffect } from 'react'
+import Data from './Data'
+export default function Doll() {
+  const [save, setsave] = useState(Data)
+ 
 
- function Main() {
-const [saves,setSaves]=useState([]);
-const [fetcherror,setFetchError]=useState(null)
-
-
-useEffect (()=>{
-  fetch("https://api.github.com/users").then((result)=>{
-    result.json().then((resp)=>
-    {
-      console.log("result",resp);
-      setSaves(resp);
-    })
+  useEffect(() => {
+    {Object.keys(save[1]).map((a)=>console.log(a))
+      
     }
-    )
-},[])
+    console.log("clicked");
+  }, [])
+  
   return (
-    <div>Main
-GET API
+    <div>  <div  style={{margin:"auto"}}>API List of Users</div>    
+<div className='kissa' style={{display:"flex",marginLeft:"30px",width:"6000px"}}>
+<div style={{fontSize:"16px"}}><b>S.no</b></div>
+{Object.keys(save[0]).map((item)=>
+{ return(
+    <div  style={{display:"flex",marginLeft:"250px",fontSize:"18px"}}>
+      {item}
+      </div> 
+)
+})}
 
-<div className='tablo'>
-    <table >
-<thead  className='kiss'>
-    <tr  className='kiss'>
-        <th className='kiss' style={{marginLeft:"40px"}} >S.no</th>
-        <th className='kiss'>Login</th>
-        
-        <th    className='kiss'> _Id_ </th>
-        <th className='kiss'> Node_Id </th>
-        <th className='kiss'>avatar_url</th>
-        <th className='kiss'>Gravatar_Id</th>
-        <th className='kiss'>Url
-    </th>
 
-        <th className='kiss'>Html_Url</th>
-        
-        <th    className='kiss'>Follower_Url</th>
-        <th className='kiss'>Following_Url</th>
-        <th className='kiss'>Gists_Url</th>
-        <th className='kiss'>starred_url</th>
-        <th className='kiss'>Subscriptions_url</th>
 
-        <th className='kiss'>Org</th>
-        
-        <th    className='kiss'>Repos_url</th>
-        <th className='kiss'>Events_url</th>
-        <th className='kiss'>Recieved_events_url</th>
-        <th className='kiss'>Type</th>
-        <th className='kiss'>Site_admin</th>
-    </tr>
-</thead>
-<tbody>
 
-{
-        saves.map((bata,index)=>{
-// TotalPrice+=bata.Price*1;
-    return (
-        <tr key={index}>
+<div style={{display:"flex"}}>
+</div>
+
+</div>
+
+<div style={{marginLeft:"40px",display:"fle"}}>
+{ save.map((bata,index)=>{
+  return(
+ 
+           
+           <tr key={index}>
            
          
-            <th className='kiss1' style={{backgroundColor:"white"}}> {index+1}</th>
+           <th className='kiss1' style={{backgroundColor:"white"}}> {index+1}</th>
+      
+           <td className='kis' style={{marginLeft:"40px"}}><b style={{marginLeft:"250px"}}>  {bata.login}</b></td>
+       <td className='kiss1' style={{marginLeft:"40px"}}><b style={{marginLeft:"210px"}}> {bata.id}     </b></td>
+           <td className='kiss1'  style={{marginLeft:"40px"}}><b style={{marginLeft:"250px"}}>  {bata.node_id} </b></td>
+           <td className='kiss1'><b style={{marginLeft:"110px"}}>  {bata.avatar_url} </b></td>
+           <td className='kiss1'><b style={{marginLeft:"170px"}}>  {bata.gravatar_id}    </b></td>
+           <td className='kiss1'><b style={{marginLeft:"300px"}}>  {bata.url}</b></td>
+           <td className='kiss1'><b style={{marginLeft:"110px"}}>  {bata.html_url}</b></td>
+           <td className='kiss1'><b style={{marginLeft:"110px"}}>  {bata.followers_url}</b></td>
+       <td className='kiss1'><b style={{marginLeft:"70px"}}> {bata.following_url}     </b></td>
+           <td className='kiss1'><b style={{marginLeft:"20px"}}>  {bata.gists_url} </b></td>
+           <td className='kiss1'><b style={{marginLeft:"80px"}}>  {bata.starred_url}    </b></td>
+           <td className='kiss1'><b style={{marginLeft:"70px"}}>  {bata.subscriptions_url}</b></td>
+            <td className='kiss1'><b style={{marginLeft:"90px"}}>  {bata.organizations_url}</b></td>
+           <td className='kiss1'><b style={{marginLeft:"140px"}}>  {bata.repos_url}</b></td>
+           <td className='kiss1'><b style={{marginLeft:"80px"}}>  {bata.events_url}</b></td>
+       <td className='kiss1'><b style={{marginLeft:"70px"}}> {bata.received_events_url}     </b></td>
+           <td className='kiss1'><b style={{marginLeft:"150px"}}>  {bata.type} </b></td>
        
-            <td className='kiss1'><b>  {bata.login}</b></td>
-        <td className='kiss1'><b> {bata.id}     </b></td>
-            <td className='kiss1'><b>  {bata.node_id} </b></td>
-            <td className='kiss1'><b>  {bata.avatar_url} </b></td>
-            <td className='kiss1'><b>  {bata.gravatar_id}    </b></td>
-            <td className='kiss1'><b>  {bata.url}</b></td>
-            <td className='kiss1'><b>  {bata.html_url}</b></td>
-            <td className='kiss1'><b>  {bata.followers_url}</b></td>
-        <td className='kiss1'><b> {bata.following_url}     </b></td>
-            <td className='kiss1'><b>  {bata.gists_url} </b></td>
-            <td className='kiss1'><b>  {bata.starred_url}    </b></td>
-            <td className='kiss1'><b>  {bata.subscriptions_url}</b></td>
-             <td className='kiss1'><b>  {bata.organizations_url}</b></td>
-            <td className='kiss1'><b>  {bata.repos_url}</b></td>
-            <td className='kiss1'><b>  {bata.events_url}</b></td>
-        <td className='kiss1'><b> {bata.received_events_url}     </b></td>
-            <td className='kiss1'><b>  {bata.type} </b></td>
-            {/* <td className='kiss1'><b>  {bata.type}    </b></td> */}
-            <td className='kiss1'><b>  {bata.site_admin}</b></td>
-            {/* <td><b>  {bata.html_url}</b></td> */}
-         
-            </tr>
-        )
-        }
-        )
-        }
+           <td className='kiss1'><b>  {bata.site_admin}</b></td>
         
-</tbody>
-</table>
-{/* <div  className='totals'style={{marginLeft:"118px"}}>
-    <h>{<b>Total: 
-        <h  style={{ backgroundColor:"yellow" ,color:"black", marginLeft:"32px",height:"10px"}} >
-             {TotalPrice}</h> </b>}</h></div> */}
+        
+           </tr>
+             
+  )
+})} 
 </div>
+
+
     </div>
   )
 }
-export default Main;
+
+
